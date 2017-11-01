3f84bd42442cc5fd071e3b7c5505e85ea31cebf4
Seven_Tao <siweitao@siweitao.com>
2017-11-01 23:10:00 +0800

    把vue从dll打包中去除，devtool无法使用

--- build/webpack.dll.conf.js
+++ build/webpack.dll.conf.js
@@ -8,9 +8,10 @@ const package = require('../package.json')
 const AssetsPlugin = require('assets-webpack-plugin')
 module.exports = {
   entry: {
-    //读取package.json里的依赖，normalize.css和mint除外
+    //读取package.json里的依赖，normalize.css除外，打包会报错
+    //如果使用了chrome的vue-devtool，那打包的时候把vue也排除掉，因为压缩过的vue是不能使用vue-devtool的
     vendor: Object.keys(package.dependencies).filter((item) => {
-      return item.indexOf('normalize') < 0 && item.indexOf('mint') < 0
+      return item.indexOf('normalize') < 0 && item == 'vue'
     })
   },
   output: {
